251380fae377c8b2cc9bb973639abbf260beca4b
kimsunnny <60125850+kimsunnny@users.noreply.github.com>
2020-12-15 17:23:47 +0900

    Add files via upload

--- eee_html/js/common.js
+++ eee_html/js/common.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
     //visual
-    $('.visual_slide').slick({
-        arrows: false,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnHover: false
+    // $('.visual_slide').slick({
+    //     arrows: false,
+    //     adaptiveHeight: true,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     pauseOnHover: false
+    // });
+    
+    //swiper main_slide
+    var swiper = new Swiper('.swiper-container.visual_slide', {
+        hashNavigation: {
+          watchState: true,
+        },
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   clickable: true,
+        // },
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false,
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar'
+        },
+        speed: 800,
+        parallax: true
     });
-    
-    
     
 })
 
