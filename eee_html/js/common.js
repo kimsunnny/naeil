8787b42bc954f629a17b611a5e2dcfe30d04e787
kimsunnny <60125850+kimsunnny@users.noreply.github.com>
2020-12-24 01:56:38 +0900

    Add files via upload

--- eee_html/js/common.js
+++ eee_html/js/common.js
@@ -19,11 +19,44 @@ $(document).ready(function(){
       $('body').toggleClass('menu_on');
     })
 
-    $('.header_menu_toggle.on, .nav_dim').on('click', function(){
+    $('.header_menu_toggle.menu_on, .nav_dim').on('click', function(){
       $('.header_menu_toggle').removeClass('menu_on');
       $('body').removeClass('menu_on');
     })
+
+    //mobile_menu_lnb
+    $('.gnb').on('click',function(){
+      var WindowWidth = $(window).width();
+      if(WindowWidth > 960){return;}
+      $(this).toggleClass('menu_on')
+      if($(this).hasClass('menu_on')){
+          $('.lnb').slideUp('fast');
+          $(this).siblings('.lnb').slideDown('fast');
+          $('.gnb').removeClass('menu_on')
+          $(this).addClass('menu_on');
+      }else{
+          $(this).siblings('.lnb').slideUp('fast');
+          }
+      })
     
+    //사이즈조정시 메뉴 원상복구
+    $(window).resize(function(){
+      var WindowWidth = $(window).width();
+      if(WindowWidth > 320){
+          $('body').removeClass('menu_on');
+          $('.nav_dim').removeClass('menu_on');
+          $('.lnb').removeAttr('style').hide();
+        }
+      })
+
+      faqFn();
+      function faqFn() {
+        $('.faq_list li').on('click', function(){
+          $(this).siblings().find('.a_box').slideUp(300);
+          $(this).find('.a_box').slideToggle(300);
+          $(this).toggleClass('active').siblings().removeClass('active');
+        });
+      }
 })
 
 
