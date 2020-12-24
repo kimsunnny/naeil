11a035b0db4a68dd5e22878eae132f2c660343c1
kimsunnny <60125850+kimsunnny@users.noreply.github.com>
2020-12-24 11:48:32 +0900

    Add files via upload

--- eee_html/js/common.js
+++ eee_html/js/common.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-    
-    //swiper main_slide
-    var swiper = new Swiper('.swiper-container.visual_slide', {
+  MobileGnb();
+  MobileLnb();
+  if( $('#visual').length > 0 ){
+		MainSlide(); // main visual
+	}
+  if( $('#info').length > 0 ){
+		InfoAccordion(); // info accordion
+	}
+
+    //swiper 메인 비주얼
+    function MainSlide(){
+      var swiper = new Swiper('.swiper-container.visual_slide', {
         hashNavigation: {
           watchState: true,
         },
@@ -10,53 +19,62 @@ $(document).ready(function(){
           disableOnInteraction: false,
         },
         speed: 800,
-        parallax: true
-    });
+        parallax: true,
+        loop: true,
+      });
+    }
+    
+    //모바일 메뉴 gnb
+    function MobileGnb(){
+      $('.header_menu_toggle').on('click', function(){
+        $('.header_menu_toggle').toggleClass('menu_on');
+        $('body').toggleClass('menu_on');
+      })
+  
+      $('.header_menu_toggle.menu_on, .nav_dim').on('click', function(){
+        $('.header_menu_toggle').removeClass('menu_on');
+        $('body').removeClass('menu_on');
+      });
+    }
 
-    //mobile_menu_toggle
-    $('.header_menu_toggle').on('click', function(){
-      $('.header_menu_toggle').toggleClass('menu_on');
-      $('body').toggleClass('menu_on');
-    })
-
-    $('.header_menu_toggle.menu_on, .nav_dim').on('click', function(){
-      $('.header_menu_toggle').removeClass('menu_on');
-      $('body').removeClass('menu_on');
-    })
-
-    //mobile_menu_lnb
-    $('.gnb').on('click',function(){
-      var WindowWidth = $(window).width();
-      if(WindowWidth > 960){return;}
-      $(this).toggleClass('menu_on')
-      if($(this).hasClass('menu_on')){
+    //모바일 메뉴 lnb
+    function MobileLnb(){
+      $('.gnb').on('click',function(){
+        var WindowWidth = $(window).width();
+        if(WindowWidth > 960){return;}
+        $(this).toggleClass('menu_on')
+        if($(this).hasClass('menu_on')){
           $('.lnb').slideUp('fast');
           $(this).siblings('.lnb').slideDown('fast');
           $('.gnb').removeClass('menu_on')
           $(this).addClass('menu_on');
-      }else{
+        }else{
           $(this).siblings('.lnb').slideUp('fast');
-          }
-      })
-    
-    //사이즈조정시 메뉴 원상복구
-    $(window).resize(function(){
-      var WindowWidth = $(window).width();
-      if(WindowWidth > 320){
-          $('body').removeClass('menu_on');
-          $('.nav_dim').removeClass('menu_on');
-          $('.lnb').removeAttr('style').hide();
         }
-      })
-
-      faqFn();
-      function faqFn() {
-        $('.faq_list li').on('click', function(){
-          $(this).siblings().find('.a_box').slideUp(300);
-          $(this).find('.a_box').slideToggle(300);
-          $(this).toggleClass('active').siblings().removeClass('active');
-        });
-      }
+      });
+    }
+    
+    //모바일 메뉴 사이즈조정시 메뉴 닫히기
+    function MobileMenuResize(){
+      $(window).resize(function(){
+        var WindowWidth = $(window).width();
+        if(WindowWidth > 320){
+           $('body').removeClass('menu_on');
+           $('.nav_dim').removeClass('menu_on');
+           $('.lnb').removeAttr('style').hide();
+         }
+       })
+    }
+     
+    //info 아코디언 게시글
+    function InfoAccordion() {
+      $('.faq_list li').on('click', function(){
+        $(this).siblings().find('.a_box').slideUp(300);
+        $(this).find('.a_box').slideToggle(300);
+        $(this).toggleClass('active').siblings().removeClass('active');
+      });
+    }
+    
 })
 
 
